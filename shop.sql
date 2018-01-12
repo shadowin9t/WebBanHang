814f460d9df1e454f67e7926f1790f3cf5ad8246
trunghieu140596 <trunghieu140596@users.noreply.github.com>
2018-01-12 20:02:07 +0700

    xnxx

--- shop.sql
+++ shop.sql
@@ -1,64 +1,71 @@
-﻿
-create database shop;
-use shop;
-create table tb_user(
-username char(20) primary key,
-password nvarchar(256),
-firstname nvarchar(30),
-lastname nvarchar(30),
-email nvarchar(50)
-);
-create table tb_status(
-statusid int primary key,
-statusname nvarchar(30)
-);
+﻿CREATE DATABASE WebBanHang;
+GO
+USE WebBanHang;
+CREATE TABLE tb_user
+    (
+      username CHAR(20) PRIMARY KEY ,
+      password NVARCHAR(256) ,
+      firstname NVARCHAR(30) ,
+      lastname NVARCHAR(30) ,
+      email NVARCHAR(50)
+    );
+CREATE TABLE tb_status
+    (
+      statusid INT PRIMARY KEY ,
+      statusname NVARCHAR(30)
+    );
 
-drop table tb_product;
-drop table tb_category;
+INSERT  INTO tb_status
+VALUES  ( 1, N'Đang dùng' );
+INSERT  INTO tb_status
+VALUES  ( 2, N'Khóa' );
 
-insert into tb_status values(1,N'Đang dùng');
-insert into tb_status values(2, N'Khóa');
+CREATE TABLE tb_category
+    (
+      categoryid CHAR(10) PRIMARY KEY ,
+      categoryname NVARCHAR(30) ,
+      discription NVARCHAR(500) ,
+      categoryimage NVARCHAR(200) ,
+      createddate DATETIME ,
+      username CHAR(20) FOREIGN KEY REFERENCES tb_user ( username ) ,
+      statusid INT FOREIGN KEY REFERENCES tb_status ( statusid )
+    );
+CREATE TABLE tb_product
+    (
+      productid CHAR(10) PRIMARY KEY ,
+      productname NVARCHAR(30) ,
+      feature BIT ,
+      shortdiscription NVARCHAR(200) ,
+      discription NVARCHAR(500) ,
+      price FLOAT ,
+      finalprice FLOAT ,
+      displayimage NVARCHAR(200) ,
+      quantity INT ,
+      statusid INT FOREIGN KEY REFERENCES tb_status ( statusid ) ,
+      createdby CHAR(20) FOREIGN KEY REFERENCES tb_user ( username ) ,
+      createddate DATETIME ,
+      categoryid CHAR(10) FOREIGN KEY REFERENCES tb_category ( categoryid )
+    );
 
-create table tb_category(
-categoryid char(10) primary key,
-categoryname nvarchar(30),
-discription nvarchar(500),
-categoryimage nvarchar(200),
-createddate datetime,
-username char(20) foreign key references tb_user(username),
-statusid int foreign key references tb_status(statusid)
-);
-create table tb_product(
-productid char(10) primary key,
-productname nvarchar(30),
-feature bit,
-shortdiscription nvarchar(200),
-discription nvarchar(500),
-price float,
-finalprice float,
-displayimage nvarchar(200),
-quantity int,
-statusid int foreign key references tb_status(statusid),
-createdby char(20) foreign key references tb_user(username),
-createddate datetime,
-categoryid char(10) foreign key references tb_category(categoryid)
-);
-
-create table tb_permission(
-permissionid char(30) primary key,
-permissionname nvarchar(255)
-);
+CREATE TABLE tb_permission
+    (
+      permissionid CHAR(30) PRIMARY KEY ,
+      permissionname NVARCHAR(255)
+    );
 
 
 
-create table tb_permission_user(
-permissionid char(30) foreign key references tb_permission(permissionid),
-username char(20) foreign key references tb_user(username),
-primary key (permissionid, username)
-);
-select * from tb_permission_user;
+CREATE TABLE tb_permission_user
+    (
+      permissionid CHAR(30)
+        FOREIGN KEY REFERENCES tb_permission ( permissionid ) ,
+      username CHAR(20) FOREIGN KEY REFERENCES tb_user ( username ) ,
+      PRIMARY KEY ( permissionid, username )
+    );
+SELECT  *
+FROM    tb_permission_user;
 
-INSERT INTO dbo.tb_user
+INSERT  INTO dbo.tb_user
         ( username ,
           password ,
           firstname ,
@@ -73,46 +80,56 @@ VALUES  ( 'admin' , -- username - char(20)
         )
 
 
-insert into tb_permission values('SUPER_ADMIN',N'Quyền quản trị hệ thống');
-insert into tb_permission values('USER_ADMIN',N'Quyền quản lí người dùng');
-insert into tb_permission values('PRODUCT_ADMIN',N'Quyền quản lí sản phẩm');
-insert into tb_permission values('ORDER_ADMIN', N'Quyền quản lí đơn đặt hàng');
-insert into tb_permission values('ARTICLE_ADMIN', N'Quyền quản lí bài viết');
+INSERT  INTO tb_permission
+VALUES  ( 'SUPER_ADMIN', N'Quyền quản trị hệ thống' );
+INSERT  INTO tb_permission
+VALUES  ( 'USER_ADMIN', N'Quyền quản lí người dùng' );
+INSERT  INTO tb_permission
+VALUES  ( 'PRODUCT_ADMIN', N'Quyền quản lí sản phẩm' );
+INSERT  INTO tb_permission
+VALUES  ( 'ORDER_ADMIN', N'Quyền quản lí đơn đặt hàng' );
+INSERT  INTO tb_permission
+VALUES  ( 'ARTICLE_ADMIN', N'Quyền quản lí bài viết' );
 
-insert into tb_permission_user values('SUPER_ADMIN','admin');
+INSERT  INTO tb_permission_user
+VALUES  ( 'SUPER_ADMIN', 'admin' );
 
 
-CREATE TABLE tb_Customer(
-	CustomerId NVARCHAR(30) PRIMARY KEY,
-	Phone NVARCHAR(30),
-	Adress NVARCHAR(200),
-	FirtName NVARCHAR(30),
-	LastName NVARCHAR(30),
-	CreatedDate DATETIME,
-	Email NVARCHAR(30),
-	Pass NVARCHAR(256)
-);
+CREATE TABLE tb_Customer
+    (
+      CustomerId NVARCHAR(30) PRIMARY KEY ,
+      Phone NVARCHAR(30) ,
+      Adress NVARCHAR(200) ,
+      FirtName NVARCHAR(30) ,
+      LastName NVARCHAR(30) ,
+      CreatedDate DATETIME ,
+      Email NVARCHAR(30) ,
+      Pass NVARCHAR(256)
+    );
 
-CREATE TABLE tb_OrderStatus(
-	OrderStatusId NVARCHAR(10) PRIMARY KEY,
-	OrderStatusName NVARCHAR(30)
-);
+CREATE TABLE tb_OrderStatus
+    (
+      OrderStatusId NVARCHAR(10) PRIMARY KEY ,
+      OrderStatusName NVARCHAR(30)
+    );
 
-CREATE TABLE tb_Order(
-	OrderId NVARCHAR(10) PRIMARY KEY,
-	CreatedDate DATETIME,
-	OrderDate DATETIME,
-	CustomerId NVARCHAR(30) NOT NULL,
-	OrderAdrees NVARCHAR(200),
-	StatusOrderId NVARCHAR(10) NOT NULL,
-	FOREIGN KEY (CustomerId) REFERENCES dbo.tb_Customer(CustomerId),
-	FOREIGN KEY (StatusOrderId) REFERENCES dbo.tb_OrderStatus(OrderStatusId)
-);
+CREATE TABLE tb_Order
+    (
+      OrderId NVARCHAR(10) PRIMARY KEY ,
+      CreatedDate DATETIME ,
+      OrderDate DATETIME ,
+      CustomerId NVARCHAR(30) NOT NULL ,
+      OrderAdrees NVARCHAR(200) ,
+      StatusOrderId NVARCHAR(10) NOT NULL ,
+      FOREIGN KEY ( CustomerId ) REFERENCES dbo.tb_Customer ( CustomerId ) ,
+      FOREIGN KEY ( StatusOrderId ) REFERENCES dbo.tb_OrderStatus ( OrderStatusId )
+    );
 
-CREATE TABLE tb_OrderDetail(
-	OrderId NVARCHAR(10) NOT NULL,
-	productid char(10) NOT NULL,
-	PRIMARY KEY (OrderId, productid),
-	FOREIGN KEY (OrderId) REFERENCES dbo.tb_Order(OrderId),
-	FOREIGN KEY (productid) REFERENCES dbo.tb_product(productid)
-);
+CREATE TABLE tb_OrderDetail
+    (
+      OrderId NVARCHAR(10) NOT NULL ,
+      productid CHAR(10) NOT NULL ,
+      PRIMARY KEY ( OrderId, productid ) ,
+      FOREIGN KEY ( OrderId ) REFERENCES dbo.tb_Order ( OrderId ) ,
+      FOREIGN KEY ( productid ) REFERENCES dbo.tb_product ( productid )
+    );
